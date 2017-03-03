7788198c85ab050c4df0110684adbceb0a4b98f7
pablo126 <pablo12614@gmail.com>
2017-03-03 18:38:01 +0100

    Terminado con templates blocks

--- flask_1/flask1/t1.py
+++ flask_1/flask1/t1.py
@@ -12,31 +12,29 @@ def hello_world_txt(todo):
     response.set_data("Pepe <b>eres un </b>"+todo);
     return response
 
-@app.route('/archivo')
-def enviar_archivo():
-    response = Response()
-    f = open('facturon.jpg', 'rb')
-    imagen = f.read()
-    response.headers['Content-Type'] = 'image/jpg'
-    response.set_data(imagen);
-    return response
+# @app.route('/archivo')
+# def enviar_archivo():
+#     response = Response()
+#     f = open('facturon.jpg', 'rb')
+#     imagen = f.read()
+#     response.headers['Content-Type'] = 'image/jpg'
+#     response.set_data(imagen);
+#     return response
 
 @app.route('/archivo')
-def enviar_archivo2():
-    response = Response()
-    f = open('facturon.jpg', 'rb')
-    imagen = f.read()
-    response.headers['Content-Type'] = 'image/jpg'
-    response.set_data(imagen);
-    return response
+def archivo():
+    return render_template('archivo.html')
 
-@app.route('/')
-def sitio():
+@app.route('/texto')
+def texto():
     usuarios = []
     usuarios.append({'name':'Pablo2', 'dni':'123'})
     usuarios.append({'name':'Pablo3', 'dni':'1111'})
-    return render_template('index.html', var='esto', usuarios=usuarios)
+    return render_template('texto.html', var='esto', usuarios=usuarios)
 
+@app.route('/')
+def sitio():
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
