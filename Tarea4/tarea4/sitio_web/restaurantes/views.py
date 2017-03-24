8be2ab99ef516327eca0b54d01236447f4dee261
Pablo126 <pablo12614@gmail.com>
2017-03-24 16:11:05 +0100

    Tarea 4 en proceso

---
+++ Tarea4/tarea4/sitio_web/restaurantes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+from .models import restaurants
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+def test(request):
+    context = {} #Aqui van las variables para la plantilla
+    return render(request,'test.html',context)
+
+def listar(requests):
+     context = {
+        "resta": restaurants.objectcs[:5], # los cinco primeros
+     }
+     return render (requests, 'restaurantes/listar.html', context)
