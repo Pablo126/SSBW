66e660f48944f01244a01fd1e84afeab8a5e34a3
Pablo126 <pablo12614@gmail.com>
2017-03-24 16:46:43 +0100

    Falta entrada de datos

--- Tarea4/tarea4/sitio_web/restaurantes/views.py
+++ Tarea4/tarea4/sitio_web/restaurantes/views.py
@@ -11,6 +11,6 @@ def test(request):
 
 def listar(requests):
      context = {
-        "resta": restaurants.objectcs[:5], # los cinco primeros
+        "resta": restaurants.objects[:5], # los cinco primeros
      }
      return render (requests, 'restaurantes/listar.html', context)
